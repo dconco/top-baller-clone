714f81bd4d305bda8583d4a7a10cafb193336c08
dconco <onipededavid208@gmail.com>
2024-09-23 21:34:41 +0100

    completed homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['**/*.html', '**/*.htm'],
-  theme: {
-    extend: {
-      fontFamily: {
-        Barlow: 'Barlow',
-        BarlowSemiCondensed: 'Barlow Semi Condensed',
-        inter: 'Inter',
-        cardo: 'Cardo'
-      }
-    }
-  },
-  plugins: []
+	content: ['**/*.html', '**/*.htm'],
+	theme: {
+		extend: {
+			fontFamily: {
+				Barlow: 'Barlow',
+				BarlowSemiCondensed: 'Barlow Semi Condensed',
+				inter: 'Inter',
+				cardo: 'Cardo'
+			}
+		}
+	},
+	plugins: []
 }
